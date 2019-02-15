67e0fe3722dac6258b626a63eb38ef8fb441bf31
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2019-02-15 01:46:47 -0500

    Ready for Deploy

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const Navbar = () => (
  <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
-import Navbar from "../components/Navbar";
-import Layout from "../components/Layout";
+import Navbar from '../components/Navbar';
+import Layout from '../components/Layout';
 
 const About = () => (
   <div>
