e0f1d05e84c305c17576cb7be0f062f1768e3da5
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2019-03-08 08:25:05 -0500

    Disable module cache

--- server.js
+++ server.js
@@ -13,16 +13,12 @@ app.use(
   }),
 );
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
   app.use(express.static('build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname,'build', 'index.html'));
-  });
-}
+  
 
 // Listen to whatever port above.
 app.listen(PORT, () => {
