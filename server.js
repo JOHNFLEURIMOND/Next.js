4df4d4c3d084efa651fe69a76b3aea9de43e2568
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2019-03-08 08:02:08 -0500

    Adapt build scripts to Heroku changes

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 8888;
 const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname)));
-app.get('/ping', function (req, res) {
-  return res.send('pong');
-});
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(bodyParser.json()); // get information from html forms
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'build', 'index.html'));
+  });
+}
 
 // Listen to whatever port above.
 app.listen(PORT, () => {
